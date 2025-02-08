5a66483d2747fde88bac834418bb7f22cc6a7b10
Mohamed shosha <moshosha267@gmail.com>
2025-02-08 14:34:20 +0200

    First Commit

---
+++ src/main/java/com/shosha/springboot/demo/jopportal/JopPortalApplication.java
@@ -0,0 +1,13 @@
+package com.shosha.springboot.demo.jopportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JopPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JopPortalApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shosha/springboot/demo/jopportal/JopPortalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shosha.springboot.demo.jopportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JopPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
