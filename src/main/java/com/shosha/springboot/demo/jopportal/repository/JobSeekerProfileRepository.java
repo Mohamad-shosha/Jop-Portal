6a642fb15de3c0744fe16bd0ee6ab6308d15035a
Mohamed shosha <moshosha267@gmail.com>
2025-02-18 01:07:30 +0200

    Add password encoding during user creation
    Integrated PasswordEncoder into UsersServiceImp to hash passwords before saving users. This enhances security by ensuring stored passwords are encrypted. Updated constructor to inject PasswordEncoder and modified addNew method to apply encoding.
    
    Signed-off-by: Mohamed shosha <moshosha267@gmail.com>

--- src/main/java/com/shosha/springboot/demo/jopportal/repository/JobSeekerProfileRepository.java
+++ src/main/java/com/shosha/springboot/demo/jopportal/repository/JobSeekerProfileRepository.java
@@ -2,6 +2,8 @@ package com.shosha.springboot.demo.jopportal.repository;
 
 import com.shosha.springboot.demo.jopportal.entity.JobSeekerProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface JobSeekerProfileRepository extends JpaRepository<JobSeekerProfile, Integer> {
 }
--- src/main/java/com/shosha/springboot/demo/jopportal/service/UsersService.java
+++ src/main/java/com/shosha/springboot/demo/jopportal/service/UsersService.java
@@ -6,5 +6,8 @@ import java.util.Optional;
 
 public interface UsersService {
     Users addNew(Users user);
+
     Optional<Users> getUserByEmail(String username);
+
+    Object getCurrentUserProfile();
 }
